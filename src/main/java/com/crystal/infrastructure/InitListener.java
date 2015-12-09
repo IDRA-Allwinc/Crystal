be73d1fa0bc40c681dadf660778cc46b5c6fe30e
Israel Vite Silva <israelvi@hotmail.com>
2015-12-09 13:10:14 -0600

    Fase inicial

---
+++ src/main/java/com/crystal/infrastructure/InitListener.java
@@ -0,0 +1,12 @@
+package com.crystal.infrastructure;
+
+import javax.servlet.ServletContextEvent;
+
+public class InitListener implements javax.servlet.ServletContextListener {
+
+    public void contextDestroyed(ServletContextEvent arg0) {
+    }
+    public void contextInitialized(ServletContextEvent arg0) {
+        HibernatePersistenceProviderResolver.register();
+    }
+}
---
+++ src/main/java/com/crystal/model/entities/shared/EntityGrid.java
@@ -0,0 +1,5 @@
+package com.crystal.model.entities.shared;
+
+public interface EntityGrid {
+    public Long getId();
+}
