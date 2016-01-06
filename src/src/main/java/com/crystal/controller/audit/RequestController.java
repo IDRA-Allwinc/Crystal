0c17b15085892bf58abc363c8017a1ea3f9c1fb8
Israel Vite Silva <israelvi@hotmail.com>
2016-01-05 20:08:21 -0600

    Corrección

--- src/src/main/java/com/crystal/controller/audit/RequestController.java
+++ src/src/main/java/com/crystal/controller/audit/RequestController.java
@@ -3,15 +3,15 @@ package com.crystal.controller.audit;
 import com.crystal.model.entities.audit.view.RequestView;
 import com.crystal.service.shared.GridService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Administrator on 1/5/2016.
  */
-@Controller
+@RestController
 public class RequestController {
 
     @Autowired
