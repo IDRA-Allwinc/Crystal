b252a7c54951afc1cf6881c4f0a96f42f9637a9a
Israel Vite Silva <israelvi@hotmail.com>
2016-01-07 13:21:15 -0600

    Corrección

--- src/src/main/java/com/crystal/model/entities/audit/view/LetterView.java
+++ src/src/main/java/com/crystal/model/entities/audit/view/LetterView.java
@@ -29,4 +29,36 @@ public class LetterView {
     private String description;
 
     private String color;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
