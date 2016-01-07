d1ebf4bd0d1e24991ecda3581d72369b04efe26c
Israel Vite Silva <israelvi@hotmail.com>
2016-01-07 13:09:48 -0600

    Corrección

--- src/src/main/java/com/crystal/model/entities/account/UserView.java
+++ src/src/main/java/com/crystal/model/entities/account/UserView.java
@@ -1,5 +1,6 @@
 package com.crystal.model.entities.account;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.annotations.Subselect;
 
 import javax.persistence.Entity;
@@ -20,4 +21,52 @@ public class UserView{
     private String role;
 
     private Boolean enabled;
+
+    public Long getId() {
+        return null;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
 }
